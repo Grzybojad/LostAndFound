ba08449c4e0aa5882f3c6950111d74fb64ff09f5
Adam <grzybojad@gmail.com>
2021-01-28 14:28:34 +0100

    Changed to URP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Vector3 moveDir;
+	Quaternion targetRot;
+
+	[SerializeField] float moveForce = 1000;
+	[SerializeField] float maxAngle = 100;
+	[SerializeField] float rotationTime = 0.5f;
+
+	float rotationVelocity;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		targetRot = transform.rotation;
+	}
+	
+	void Update()
+	{
+		// moveDir *= 0.9f;
+	}
+
+	void FixedUpdate()
+	{
+		MoveRb();
+	}
+
+
+	void MoveRb()
+	{
+		rb.AddForce( moveDir * (moveForce * Time.fixedDeltaTime) );
+		rb.MoveRotation( targetRot );
+	}
+
+	public void OnMove( InputValue value )
+	{
+		Vector2 inputDir = value.Get<Vector2>();
+		// Quaternion rot = transform.rotation;
+
+		moveDir = inputDir;
+
+		// moveDir = (transform.up * inputDir.y).normalized;
+		//
+		// float currentAngle = rot.eulerAngles.z;
+		// float targetAngle = inputDir.x * maxAngle;
+		// float newAngle = Mathf.SmoothDampAngle( currentAngle, targetAngle, ref rotationVelocity, rotationTime );
+		//
+		// Debug.Log( targetAngle );
+		//
+		// targetRot = Quaternion.Euler( rot.x, rot.y, newAngle );
+	}
 }
